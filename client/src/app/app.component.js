6a9babd91c2d6794b98099c25d8d8efc261455b9
saguo <saguo@umich.edu>
2017-07-30 13:04:54 -0700

    basic frame

--- client/src/app/app.component.js
+++ client/src/app/app.component.js
@@ -11,12 +11,13 @@ var __metadata = (this && this.__metadata) || function (k, v) {
 var core_1 = require('@angular/core');
 var AppComponent = (function () {
     function AppComponent() {
-        this.name = 'Angular';
     }
     AppComponent = __decorate([
         core_1.Component({
+            moduleId: module.id,
             selector: 'my-app',
-            template: "<h1>Hello {{name}}</h1>",
+            templateUrl: './app.component.html',
+            styleUrls: ['./app.component.css'],
         }), 
         __metadata('design:paramtypes', [])
     ], AppComponent);
